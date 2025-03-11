866999f146f807faf2a32be6f9d72cfe5f9cae97
samgiz87 <semogiz87@gmail.com>
2025-03-11 12:07:30 -0400

    Deploy on Netlify

--- Amazon-Part1/src/Api/axios.js
+++ Amazon-Part1/src/Api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
  const axiosInstance = axios.create ({
    // deploy version of Firebase Function
-    baseURL:"https://api-j62kbgz23a-uc.a.run.app",
+    // baseURL:"https://api-j62kbgz23a-uc.a.run.app",
 
    //  deploy version of render version 
     baseURL:"https://amazon-api-deploy-jlv8.onrender.com"
