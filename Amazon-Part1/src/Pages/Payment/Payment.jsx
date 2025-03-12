6f6d87c2a4e880836fdd2c70c557080934443615
samgiz87 <semogiz87@gmail.com>
2025-03-12 15:10:27 -0400

    "Deploy to netlify"

--- Amazon-Part1/src/Pages/Payment/Payment.jsx
+++ Amazon-Part1/src/Pages/Payment/Payment.jsx
@@ -43,7 +43,7 @@ function Payment() {
         method: "POST",
         url: `/payment/create?total=${total * 100}`,
       });
-      // console.log(response.data);
+      console.log(response.data);
       const clientSecret = response.data?.clientSecret;
 
       const { paymentIntent } = await stripe.confirmCardPayment(clientSecret, {
